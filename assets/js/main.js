8e9e8bd95387a095b1f5efa3adf32aa08cdc1291
jihadph <38645633+jihadph@users.noreply.github.com>
2026-08-26 13:40:36 +0800

    Update links for Alangadi Links.

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,116 @@ const guestList = [
         guest: 'anjelikaakiko',
         name: "Anjelika & Akiko Orui and Family",
     },
+    {
+        id: 'M13',
+        guest: 'govbombit',
+        name: "Gov Mamintal 'Bombit' Alonto Adiong Jr. and Family",
+    },
+    {
+        id: 'M14',
+        guest: 'chairmanbansa',
+        name: "Chairman Bansa Alangadi - Sumpingan and Children",
+    },
+    {
+        id: 'M15',
+        guest: 'camaltata',
+        name: "Camal & Tata",
+    },
+    {
+        id: 'M16',
+        guest: 'yashmin',
+        name: "Yashmin Bayabao",
+    },
+    {
+        id: 'M17',
+        guest: 'anisa',
+        name: "Anisa Romaiza Bayabao Pundaodaya",
+    },
+    {
+        id: 'M18',
+        guest: 'vicegovnormallah',
+        name: "Vice Governor Bai Normallah Alonto Lucman - Pacasum",
+    },
+    {
+        id: 'M19',
+        guest: 'omaimah',
+        name: "Omaimah Macaraya - Lucman",
+    },
+    {
+        id: 'M20',
+        guest: 'drjamila',
+        name: "Dr. Jamila Barodi - Abdulcarim",
+    },
+    {
+        id: 'M21',
+        guest: 'drnariman',
+        name: "Dr. Nariman Lao - Taha",
+    },
+    {
+        id: 'M22',
+        guest: 'akima',
+        name: "Budget Officer Akima Macarambon - Bancola",
+    },
+    {
+        id: 'M23',
+        guest: 'drnormallah',
+        name: "Dr. Normallah Dimalotang - Alonto",
+    },
+    {
+        id: 'M24',
+        guest: 'omie',
+        name: "Ms. Omie Dalidig - Bedar",
+    },
+    {
+        id: 'M25',
+        guest: 'vicemayorminda',
+        name: "Vice Mayor Minda Bato - Lamping",
+    },
+    {
+        id: 'M26',
+        guest: 'sittiemampao',
+        name: "Sittie Mampao Mudag",
+    },
+    {
+        id: 'M27',
+        guest: 'aragasi',
+        name: "Prof. Fatima Aragasi & Chancellor Usman Aragasi (MSU Gensan)",
+    },
+    {
+        id: 'M28',
+        guest: 'aziza',
+        name: "Aziza Mamutuk Alonto",
+    },
+    {
+        id: 'M29',
+        guest: 'almaira',
+        name: "Almaira Alangadi",
+    },
+    {
+        id: 'M30',
+        guest: 'saharaampuan',
+        name: "Sahara Alangadi Ampuan",
+    },
+    {
+        id: 'M31',
+        guest: 'drsophia',
+        name: "Dr. Sophia Alonto Ampuan Sharief",
+    },
+    {
+        id: 'M32',
+        guest: 'dramer',
+        name: "Dr. Amer Decampong Jr.",
+    },
+    {
+        id: 'M33',
+        guest: 'asnim',
+        name: "Asnim Ali Decampong",
+    },
+    {
+        id: 'M34',
+        guest: 'rvgharon',
+        name: "RVG Haron Al Rashid Lucman Jr.",
+    },
 
     /* FIGUERAS FAMILY */
 
